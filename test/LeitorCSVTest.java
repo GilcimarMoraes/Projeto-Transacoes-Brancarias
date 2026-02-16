26d772d8235c362352e09958c41e55e947933a87
GilcimarMoraes <myrraws@yahoo.com.br>
2026-02-16 13:58:26 -0300

    Testando leitura do arquivo CSV

---
+++ test/LeitorCSVTest.java
@@ -0,0 +1,21 @@
+import model.Transacao;
+import service.LeitorCSV;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeitorCSVTest {
+
+    public static void main(String[] args) {
+
+        LeitorCSV leitorCSV = new LeitorCSV();
+
+        List<Transacao> lista = leitorCSV.lerArquivo( "data/operacoes.csv" );
+
+        System.out.println( "\n Total lido: " + lista.size() );
+
+        for( Transacao t : lista ) {
+            System.out.println( " " + t );
+        }
+    }
+}
